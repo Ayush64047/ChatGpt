e1c7e6303642942868f0f1e74ddb8233acd648bc
Aayush <bhaskarsharma64047@gmail.com>
2023-02-10 17:05:21 +0530

    Update googleSearch.py

--- utils/googleSearch.py
+++ utils/googleSearch.py
@@ -13,7 +13,7 @@ def googleSearch(query):
     "q": query,
     "hl": "en",
     "gl": "us",
-    "api_key": os.getenv("2eefbd80-a936-11ed-a549-c3b0debeb73b")
+    "api_key": os.getenv("SERP_API_KEY")
   }
 
   search = GoogleSearch(params)
